2fb135446b0e0c8ae419305f6f4828ba8b965941
muhd haziq <blmowner@gmail.com>
2017-03-13 02:13:37 +0800

    asdasd

--- download.php
+++ download.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <?php 
+
+      // for docx
 	  $file = 'downloads/seluar.docx';
 	  header('Content-Description: File Transfer');
 	  header("Content-Type: application/vnd.openxmlformats");
@@ -13,7 +15,18 @@
 	  header('Content-Length: ' . filesize($file));
 	  readfile($file);
       exit;
+      
+      // for pdf
+  	  $file = 'downloads/seluar.pdf';
+  	  header('Content-Description: File Transfer');
+  	  header("Content-Type: application/pdf");
+  	  header('Content-Disposition: attachment; filename='.  basename($file));
+  	  header('Expires: 0');
+  	  header('Cache-Control: must-revalidate');
+  	  header('Pragma: public');
+  	  header('Content-Length: ' . filesize($file));
+  	  readfile($file);
+      exit;
    ?>
-  
 </head>
 </html>
